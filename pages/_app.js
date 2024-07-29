f8591bba247d82992f57271a9837ccf38c509f5e
jonber <jonber2185@gmail.com>
2024-07-29 09:53:02 +0900

    codeitMall

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import ThemeProvider from '@/lib/ThemeContext';
+import '@/styles/global.css';
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>JonberMall</title>
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+   
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import ProductList from '@/components/ProductList';
+import SearchForm from '@/components/SearchForm';
+import axios from '@/lib/axios';
+import styles from '@/styles/Home.module.css';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    const res = await axios.get('/products');
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>JonberMall</title>
+      </Head>
+      <SearchForm />
+      <ProductList className={styles.productList} products={products} />
+    </>
+  )
+}
